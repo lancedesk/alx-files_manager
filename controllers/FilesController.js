b3f71dca715c1f11dc2c05ad5df525c2a10b1e69
Lance Desk <lncedesk@gmail.com>
2024-06-28 09:00:01 -0700

    File data.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,6 +3,7 @@ const redisClient = require('../utils/redis');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const mime = require('mime-types');
 
 class FilesController {
     static async postUpload(req, res) {
@@ -198,6 +199,65 @@ class FilesController {
 
         const updatedFile = await dbClient.db.collection('files').findOne({ _id: fileObjectId });
         return res.status(200).json(updatedFile);
+    }
+
+    static async getFile(req, res)
+    {
+        const token = req.headers['x-token'];
+        const fileId = req.params.id;
+
+        let fileObjectId;
+
+        if (!fileId)
+        {
+            return res.status(404).json({ error: 'Not found' });
+        }
+
+        try
+        {
+            fileObjectId = dbClient.ObjectId(fileId);
+        }
+        catch (err)
+        {
+            return res.status(404).json({ error: 'Not found' });
+        }
+
+        const file = await dbClient.db.collection('files').findOne({ _id: fileObjectId });
+        
+        if (!file)
+        {
+            return res.status(404).json({ error: 'Not found' });
+        }
+
+        if (file.isPublic === false)
+        {
+            if (!token)
+            {
+                return res.status(404).json({ error: 'Not found' });
+            }
+
+            const userId = await redisClient.get(`auth_${token}`);
+            if (!userId || userId !== file.userId.toString())
+            {
+                return res.status(404).json({ error: 'Not found' });
+            }
+        }
+
+        if (file.type === 'folder')
+        {
+            return res.status(400).json({ error: "A folder doesn't have content" });
+        }
+
+        if (!fs.existsSync(file.localPath))
+        {
+            return res.status(404).json({ error: 'Not found' });
+        }
+
+        const mimeType = mime.lookup(file.name) || 'application/octet-stream';
+        const fileContent = fs.readFileSync(file.localPath);
+
+        res.setHeader('Content-Type', mimeType);
+        res.send(fileContent);
     }
 }
 
