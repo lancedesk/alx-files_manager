0d5dcf25f0c261f3f3260cd6a37b8d77e98b3cd9
Lance Desk <lncedesk@gmail.com>
2024-07-08 04:53:53 -0700

    First API.

--- worker.js
+++ worker.js
@@ -1,61 +1,56 @@
-const Bull = require('bull');
-const fs = require('fs');
-const path = require('path');
-const imageThumbnail = require('image-thumbnail');
-const dbClient = require('./utils/db');
-const nodemailer = require('nodemailer');
+import Queue from 'bull';
+import imageThumbnail from 'image-thumbnail';
+import mongodb from 'mongodb';
+import UserCollection from './utils/users';
+import fs from 'fs';
+import FilesCollection from './utils/files';
 
 // Initialize Bull queues
-const fileQueue = new Bull('fileQueue');
-const userQueue = new Bull('userQueue');
+const filesQue = new Queue('thumbnails');
+const usersQue = new Queue('Welcome Email');
+
+function getObjectId(id) {
+  return mongodb.ObjectId.isValid(id) ? new mongodb.ObjectId(id) : '';
+}
 
 // File processing queue
-fileQueue.process(async (job) => {
-    const { userId, fileId } = job.data;
+filesQue.process(async (job, done) => {
+  const { fileId, userId } = job.data;
 
-    if (!fileId) throw new Error('Missing fileId');
-    if (!userId) throw new Error('Missing userId');
+  if (!fileId) throw new Error('Missing fileId');
+  if (!userId) throw new Error('Missing userId');
 
-    const file = await dbClient.db.collection('files').findOne({ _id: fileId, userId });
-    if (!file) throw new Error('File not found');
+  const file = await FilesCollection.findOne({
+    _id: getObjectId(fileId),
+    userId: getObjectId(userId),
+  });
 
-    const sizes = [500, 250, 100];
-    const fileData = fs.readFileSync(file.localPath);
+  if (!file || !fs.existsSync(file.localPath)) throw new Error('File not found');
 
-    for (const size of sizes) {
-        const thumbnail = await imageThumbnail(fileData, { width: size });
-        const thumbnailPath = `${file.localPath}_${size}`;
-        fs.writeFileSync(thumbnailPath, thumbnail);
-    }
-});
+  if (file.type === 'image') {
+    [500, 250, 100].forEach((width) => {
+      const thumbnail = imageThumbnail(file.localPath, { width });
+      fs.writeFileSync(`${file.localPath}_${width}`, thumbnail);
+    });
 
-fileQueue.on('completed', (job, result) => {
-    console.log(`Job ${job.id} completed!`);
-});
+    console.log(`Thumbnail created for file ${fileId}`);
+  } else {
+    console.log(`File ${fileId} is not an image`);
+  }
 
-fileQueue.on('failed', (job, err) => {
-    console.log(`Job ${job.id} failed: ${err.message}`);
+  done();
 });
 
 // User welcome email queue
-userQueue.process(async (job) => {
-    const { userId } = job.data;
+usersQue.process(async (job, done) => {
+  const { userId } = job.data;
 
-    if (!userId) throw new Error('Missing userId');
+  if (!userId) throw new Error('Missing userId');
 
-    const user = await dbClient.db.collection('users').findOne({ _id: dbClient.ObjectId(userId) });
-    if (!user) throw new Error('User not found');
+  const user = await UserCollection.findOne({ _id: getObjectId(userId) });
+  if (!user) throw new Error('User not found');
 
-    // Printing the welcome email to console
-    console.log(`Welcome ${user.email}!`);
+  // Printing the welcome email to console
+  console.log(`Welcome ${user.email}!`);
+  done();
 });
-
-userQueue.on('completed', (job, result) => {
-    console.log(`Job ${job.id} completed!`);
-});
-
-userQueue.on('failed', (job, err) => {
-    console.log(`Job ${job.id} failed: ${err.message}`);
-});
-
-module.exports = { fileQueue, userQueue };
